54568a12b139fc1787aca06c0586817b83a4226e
Xavier Linn <xperrylinn@berkeley.edu>
2017-10-16 20:18:27 -0700

    Added file to code Monte Carlo simulation

---
+++ Project3.py
@@ -0,0 +1,9 @@
+# Xavier Linn
+# MSE 215, Project 3
+# A Monte Carlo Problem
+# Metropolis algorithm to compute the properties of a 2D Ising models.
+print("Hello world")
+
+# Make a 2D Grid
+# Generate random elements
+# etc..
